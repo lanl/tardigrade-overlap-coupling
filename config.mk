74041d44c8c32d770d37e097ecb3a3b31214d276
Nathan Andrew Miller <nathanm@lanl.gov>
2019-03-28 09:35:20 -0600

    Changed to reference common directories for libraries and headers

--- config.mk
+++ config.mk
@@ -12,19 +12,22 @@ CXX=/opt/moose/gcc-7.3.0/bin/g++
 # Flags for the C++ compiler
 CFLAGS=-std=gnu++11 -Wall -ansi -pedantic -O3 -I. -fmax-errors=5
 
-# Location of the Voro++ library
-VPPLIB=-L/projects/nathanm/micromorphic/voro++/voro++-0.4.6/src
-VPPINC=-I/projects/nathanm/micromorphic/voro++/voro++-0.4.6/src
-
 # Location of the Eigen library
 #EIGEN=-I/projects/nathanm/usr/local/include/eigen-git-mirror
 
 # The python command
 PYTHON=/apps/anaconda3/bin/python
 
-# The quickhull object file
-QHULL=/projects/nathanm/micromorphic/micromorphic_library/external_repositories/quickhull
+# Location of the quickhull source code
+QHULL=/projects/nathanm/micromorphic/micromorphic_library/external_repositories/quickhull/
 
 # Additional includes
-INC=-I/projects/nathanm/micromorphic/micromorphic_library/external_repositories/
-INC+=-I$(QHULL)
+ROOTDIR=/projects/nathanm/micromorphic/micromorphic_library
+INCDIR=$(ROOTDIR)/include
+LIBDIR=$(ROOTDIR)/lib
+INC=-I$(INCDIR)
+INC+=-I$(INCDIR)/voro++
+INC+=-I$(INCDIR)/overlap
+LIB=-L$(LIBDIR)
+LIB+=-L$(LIBDIR)/voro++
+LIB+=-L$(LIBDIR)/overlap
