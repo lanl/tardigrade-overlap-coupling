Corrected error in computation of shapefunction matrix

src/python/generate_example_data.py
@@ -56,7 +56,7 @@ def voigt(A):
     return np.array([A[i,j] for i,j in order])
 
 def transformation(x, t):
-    angles = np.zeros([3,])#t*np.array([np.pi/2, -np.pi/5, np.pi/3])
+    angles = t*np.array([np.pi/2, -np.pi/5, np.pi/3])
     Q = rotation_matrix(random=False, angles=angles)
     C = t*np.array([[ 1.00,  0.20, -0.05],\
                     [ 0.20, -0.40, -0.20],\