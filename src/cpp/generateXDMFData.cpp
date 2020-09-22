1b1dfe3a04bacb3588acc7f6ea871796f65ca875
Nathan Miller <nathanm@lanl.gov>
2020-09-21 23:34:29 -0600

    FEAT: Initial commit of files

---
+++ src/cpp/generateXDMFData.cpp
@@ -0,0 +1,8 @@
+/*=============================================================================
+|                              generateXDMFData.h                             |
+===============================================================================
+| Functions used to generate XDMF data which can either be used to test the   |
+| overlapCoupling class or to create filtering domains.                       |
+=============================================================================*/
+
+
