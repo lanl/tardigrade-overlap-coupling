4bbf498a9eedbe9b3beb5e26f8b002fd3a0181ec
Nathan Andrew Miller <nathanm@lanl.gov>
2021-08-11 11:58:37 -0600

    MAINT: Added the evaluation of the material model

--- src/python/read_xdmf_output.py
+++ src/python/read_xdmf_output.py
@@ -6,6 +6,9 @@ import h5py
 import xml.parsers.expat
 import xml.etree
 
+import micromorphic
+
+
 def get_attribute_data(xml_node, path='.'):
     
     if ("Attribute" != xml_node.tag):
@@ -430,3 +433,150 @@ def compute_strains(data, dim=3):
                 Gamma[qp][inc,e,:,:,:] = np.einsum('iI,iJK->IJK', F, grad_chi)
 
     return GreenLagrangeStrain, MicroGreenLagrangeStrain, Gamma
+
+def evaluate_model(data, parameters, model_name, parameters_to_fparams, nsdvs, maxinc=None, dim=3):
+    """
+    Evaluate the model given the parameters
+    
+    :param dict data: The data dictionary
+    :param np.ndarray parameters: The array of parameters
+    :param str model_name: The name of the model
+    :param func parameters_to_fparams: A function that converts the parameters vector to the fparams vector required
+        for the function
+    :param int nsdvs: The number of solution dependant state variables
+    :param int maxinc: The maximum increment to evaluate
+    :param int dim: The spatial dimension of the problem
+    """
+    
+    ninc = data['density_0'].shape[0]
+
+    nel = data['density_0'].shape[1]
+
+    nqp = len([key for key in data.keys() if 'density' in key])
+
+    position, grad_u, phi, grad_phi = construct_degrees_of_freedom(data, dim = dim)
+    
+    PK2_sim   = dict([(qp,np.zeros((ninc,nel,dim * dim))) for qp in range(nqp)])
+    SIGMA_sim = dict([(qp,np.zeros((ninc,nel,dim * dim))) for qp in range(nqp)])
+    M_sim     = dict([(qp,np.zeros((ninc,nel,dim * dim * dim))) for qp in range(nqp)])
+    SDVS_sim  = dict([(qp,np.zeros((ninc,nel,nsdvs))) for qp in range(nqp)])
+    
+    keys = ['errorCode', 'PK2', 'SIGMA', 'M', 'SDVS',\
+            'DPK2Dgrad_u', 'DPK2Dphi', 'DPK2Dgrad_phi',\
+            'DSIGMADgrad_u', 'DSIGMADphi', 'DSIGMADgrad_phi',\
+            'DMDgrad_u', 'DMDphi', 'DMDgrad_phi',\
+            'ADD_TERMS', 'ADD_JACOBIANS', 'output_message']
+    
+    time = data['time'][1:]
+    
+    if maxinc is None:
+        maxinc = ninc
+
+    tp = 0
+        
+    for e in range(nel):
+    
+        for qp in range(nqp):
+        
+            for i in range(maxinc):
+            
+                # Map the parameters vector to the function parameters
+                fparams = parameters_to_fparams(parameters)
+            
+                sp = 0
+                ds = 1.
+                
+                previous_SDVS_s = np.copy(SDVS)
+            
+                while (sp < 1.0):
+                
+                    s = sp + ds
+                
+                    time_1     = time[i]
+                    grad_u_1   = grad_u[qp][i, e, :, :]
+                    phi_1      = phi[qp][i, e, :, :]
+                    grad_phi_1 = grad_phi[qp][i, e, :, :, :]
+                    
+                    if (i == 0):
+                        time_0     = 0
+                        grad_u_0   = np.zeros((3,3))
+                        phi_0      = np.zeros((3,3))
+                        grad_phi_0 = np.zeros((3,3,3))
+                        
+                    else:
+                        time_0     = time[i-1]
+                        grad_u_0   = grad_u[qp][i-1, e, :, :]
+                        phi_0      = phi[qp][i-1, e, :, :]
+                        grad_phi_0 = grad_phi[gp][i-1, e, :, :]
+                        
+                    t                = (time_1 - time_0) * s + time_0
+                    current_grad_u   = (grad_u_1 - grad_u_0) * s + grad_u_0
+                    current_phi      = (phi_1 - phi_0) * s + phi_0
+                    current_grad_phi = (grad_phi_1 - grad_phi_0) * s + grad_phi_0
+                    
+                    tp                = (time_1 - time_0) * sp + time_0
+                    previous_grad_u   = (grad_u_1 - grad_u_0) * sp + grad_u_0
+                    previous_phi      = (phi_1 - phi_0) * sp + phi_0
+                    previous_grad_phi = (grad_phi_1 - grad_phi_0) * sp + grad_phi_0
+                    
+                    current_phi = current_phi.flatten()
+                    previous_phi = previous_phi.flatten()
+                    
+                    current_grad_phi = current_grad_phi.reshape((dim * dim, dim))
+                    previous_grad_phi = previous_grad_phi.reshape((dim * dim, dim))
+
+                    #TODO: add dof and add grad dof not currently used
+                    current_ADD_DOF = np.zeros((1))
+                    current_ADD_grad_DOF = np.zeros((1,3))
+
+                    previous_ADD_DOF = np.zeros((1))
+                    previous_ADD_grad_DOF = np.zeros((1,3))
+                    
+                    # Evaluate the model
+                    values = micromorphic.evaluate_model(model_name, np.array([t, t - tp]), fparams,\
+                                                         current_grad_u, current_phi, current_grad_phi,
+                                                         previous_grad_u, previous_phi, previous_grad_phi,
+                                                         previous_SDVS_s,\
+                                                         current_ADD_DOF, current_ADD_grad_DOF,
+                                                         previous_ADD_DOF, previous_ADD_grad_DOF)
+                    
+                    results = dict(zip(keys, values))
+                    
+                    if (results['errorCode'] == 1):
+                        print("error")
+                        ds = 0.5 * ds
+                        nsubiter += 1
+                        
+                        if (nsubiter > maxsubiter):
+                            break
+                            
+                    elif (results['errorCode'] == 2):
+                        errormessage = f"evaluate_model return error code {results['errorCode']}\n\n"
+                        errormessage += results['output_message'].decode("utf-8")
+                        raise IOError(errormessage)
+                        
+                    else:
+                        sp += ds
+                        nsubiter = 0
+                        
+                        if np.isclose(sp, 1):
+                            ds = 1
+                        else:
+                            ds = 1 - sp
+                            
+                        previous_SDVS_s = np.copy(results['SDVS'])
+                        
+                if (results['errorCode'] != 0):
+                    errormessage = f"evaluate_model returned error code {results['errorCode']}\n\n"
+                    errormessage += results['output_message'].decode('utf-8')
+                    print(parameters, 'fail')
+                    
+                    return np.nan
+                
+                PK2_sim[qp][i,e,:]   = results['PK2']
+                SIGMA_sim[qp][i,e,:] = results['SIGMA']
+                M_sim[qp][i,e,:]     = results['M']
+                SDVS                 = results['SDVS']
+                SDVS_sim[qp][i,e,:]  = results['SDVS']
+                
+    return PK2_sim, SIGMA_sim, M_sim, SDVS_sim
